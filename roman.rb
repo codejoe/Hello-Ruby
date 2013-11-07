2f5eb8147fc0344606f29a7c562e4e879efb0d83
naufal <naufal@CN-LIN.(none)>
2013-11-07 07:44:10 +0700

    menambahkan roman.rb

---
+++ roman.rb
@@ -0,0 +1,40 @@
+@data = [
+["M"  , 1000],
+["CM" , 900],
+["D"  , 500],
+["CD" , 400],
+["C"  , 100],
+["XC" ,  90],
+["L"  ,  50],
+["XL" ,  40],
+["X"  ,  10],
+["IX" ,   9],
+["V"  ,   5],
+["IV" ,   4],
+["I"  ,   1]
+]
+
+@roman = %r{^[CDILMVX]*$}
+@arabic = %r{^[0-9]*$}
+
+def toRoman(num)
+    reply = ""
+    for key, value in @data
+        count, num = num.divmod(value)
+        reply << (key * count)
+    end
+    reply
+end
+
+def toArabic(rom)
+    reply = 0
+    for key, value in @data
+        while rom.index(key) == 0
+            reply += value
+            rom.slice!(key)
+        end
+    end
+    reply
+end
+puts toRoman(4)	
+puts toArabic('IV')
